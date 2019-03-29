da08f01e1ca135a2ec228aaf1b1662386b8d7be5
likejackonfire <gravitybringsmedown@gmail.com>
2019-03-29 12:51:50 -0700

    search form created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Search from './Search';
 import './App.css';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Star Wars Database</h1>
+        <main>
+          <Search />
+        </main>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/Search.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Search.css'
+
+export default class Search extends React.Component {
+    state = {
+        input: null,
+        results: []
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+    
+        fetch(`https://swapi.co/people?search=${this.state.input}`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error;
+            }
+            return res.json();
+        })
+        .then(data => {
+            this.setState({
+                results: data.results
+            });
+        });
+    }
+
+    handleSearch = searchInput => {
+        this.setState({
+            input: searchInput
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="search-main">
+            <form className="search-form" 
+            onSubmit={e => this.handleSubmit(e)}>
+                <input
+                    className="search-input" type="text"
+                    placeholder="JarJar Binks"
+                    required
+                    onChange={e => this.handleSearch(e.target.value)}
+                   />
+                   <button type="submit" className="search-button">
+                    Use the Force
+                   </button>
+                   </form>
+
+            <ul>
+                {this.state.results.map((result, index) => {
+                    return (
+                        <li key={index}>
+                        <p>{result.name}</p>
+                        </li>
+                    );   
+                })} 
+            </ul>       
+            
+            </div>
+        );
+            }
+}
